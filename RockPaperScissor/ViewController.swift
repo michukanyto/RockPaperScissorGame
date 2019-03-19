b007fcccda19b0a5d3551baf8b0615e962148d71
Marlon <escobar.marlon@gmail.com>
2019-03-19 11:47:19 -0400

    Creation of buttons and methods

--- RockPaperScissor/ViewController.swift
+++ RockPaperScissor/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageGame:[UIImage] = [UIImage(named: "paper.png")!,
+                               UIImage(named: "rock.png")!,
+                               UIImage(named: "scissor.png")!
+    ]
 
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var machineImageView: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func rockButtonPressed(_ sender: UIButton) {
+        userImageView.image = imageGame[1]
+    }
+    
+    
+    @IBAction func paperButonPressed(_ sender: UIButton) {
+        userImageView.image = imageGame[0]
+    }
+    
+    
+    @IBAction func scissorButtonPressed(_ sender: UIButton) {
+        userImageView.image = imageGame[2]
+    }
+    
+    
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        machineImageView.image = imageGame[Int.random(in: 0...2)]
+        
+    }
 }
 
